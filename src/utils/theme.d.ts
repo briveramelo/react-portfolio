e57d43a66dc16b0b9468eabdc40653b63094a83d
Brandon Rivera-Melo <briveramelo@gmail.com>
2025-01-07 22:49:34 -0600

    unsubstantial formatting to code

--- src/utils/theme.d.ts
+++ src/utils/theme.d.ts
@@ -1,21 +1,21 @@
 // Extend the Palette interface
 declare module "@mui/material/styles" {
-    interface Palette {
-        skills: {
-            red: string;
-            orange: string;
-            green: string;
-        };
-    }
-    interface PaletteOptions {
-        skills?: {
-            red: string;
-            orange: string;
-            green: string;
-        };
-    }
+  interface Palette {
+    skills: {
+      red: string;
+      orange: string;
+      green: string;
+    };
+  }
+  interface PaletteOptions {
+    skills?: {
+      red: string;
+      orange: string;
+      green: string;
+    };
+  }
 
-    interface TypeBackground {
-        fillbar?: string;  // optional or required
-    }
+  interface TypeBackground {
+    fillbar?: string; // optional or required
+  }
 }
