9ad7cb0b987edad2ffc051d07290d4c0e9559b24
Brandon Rivera-Melo <briveramelo@gmail.com>
2025-01-25 03:43:02 -0600

    project cards are still choppy. Experimenting with an alternate approach to animation. will likely scrap

--- src/pages/sections/Projects.tsx
+++ src/pages/sections/Projects.tsx
@@ -1,5 +1,5 @@
 import React, { forwardRef, useEffect, useRef, useState } from "react";
-import { Container, Typography, Box } from "@mui/material";
+import { Container, Typography, Box, Button } from "@mui/material";
 import { ProjectCard } from "../components/ProjectCard";
 import { ProjectDetails } from "../components/ProjectDetails";
 import { Project, projectData } from "../../data/projectData";
@@ -23,9 +23,15 @@ export const Projects = forwardRef<HTMLElement, ProjectsProps>(
     const [isProjectSelected, setIsProjectSelected] = useState<boolean>(false); // keeping separate from 'selectedProject === null' supports transition state nuances
     const [animationComplete, setAnimationComplete] = useState<boolean>(true);
     const slideDurationMs = 750;
+    const minHeight = 200;
+    const bonusHeight = 1300;
 
     const [detailsHeight, setDetailsHeight] = useState(0);
+    const [projectsHeight, setProjectsHeight] = useState(0);
+    const [headerHeight, setHeaderHeight] = useState(0);
+    const projectsRef = useRef<HTMLDivElement>(null);
     const detailsRef = useRef<HTMLDivElement>(null);
+    const headerRef = useRef<HTMLDivElement>(null);
     const sectionRef = ref as React.RefObject<HTMLElement>;
 
     const isSectionVisibleLead = useIntersectionObserver(sectionRef, {
@@ -34,6 +40,19 @@ export const Projects = forwardRef<HTMLElement, ProjectsProps>(
     const [isSectionVisibleLag, setIsSectionVisibleLag] =
       useState<boolean>(false); //lagging measure of if the section is visible
 
+    useEffect(() => {
+      window.addEventListener("resize", updateHeights);
+      return () => {
+        window.removeEventListener("resize", updateHeights);
+      };
+    }, []);
+    useEffect(()=>{
+      if(headerRef?.current){
+        setHeaderHeight(headerRef.current.scrollHeight);
+      }
+    },[headerRef])
+
+    // handle slide animation vars
     useEffect(() => {
       setAnimationComplete(false);
       setIsSectionVisibleLag(isSectionVisibleLead);
@@ -45,19 +64,26 @@ export const Projects = forwardRef<HTMLElement, ProjectsProps>(
       return () => clearTimeout(timeoutId);
     }, [isSectionVisibleLead, slideDurationMs]);
 
+    // handle project selection scrolling
     useEffect(() => {
       if (isProjectSelected && sectionRef.current) {
         sectionRef.current.scrollIntoView({ behavior: "smooth" });
       }
     }, [isProjectSelected, sectionRef]);
 
+    // handle project selection height adjustments
+    const updateHeights = () => {
+      if (projectsRef.current && detailsRef.current && headerRef.current) {
+        setDetailsHeight(detailsRef.current.scrollHeight || 0);
+        setProjectsHeight(isProjectSelected ? 0 : projectsRef.current.scrollHeight || 0);
+        setHeaderHeight(headerRef.current.scrollHeight || 0);
+      }
+    };
+
     useEffect(() => {
-      const newHeight =
-        isProjectSelected && detailsRef.current
-          ? detailsRef.current.scrollHeight
-          : 0;
-      setDetailsHeight(newHeight);
+      updateHeights();
     }, [isProjectSelected]);
+
 
     const handleCardClick = (project: Project) => {
       setSelectedProject(project);
@@ -85,14 +111,6 @@ export const Projects = forwardRef<HTMLElement, ProjectsProps>(
       }
     }, [isProjectSelected, ref]);
 
-    useEffect(() => {
-      const newHeight =
-        isProjectSelected && detailsRef?.current
-          ? detailsRef.current.scrollHeight
-          : 0;
-      setDetailsHeight(newHeight);
-    }, [isProjectSelected]);
-
     return (
       <Box
         component="section"
@@ -103,12 +121,17 @@ export const Projects = forwardRef<HTMLElement, ProjectsProps>(
           color: textColor,
           position: "relative",
           overflow: "hidden",
+          minHeight: `${minHeight}px`,
+          maxHeight: isProjectSelected
+            ? `${headerHeight + detailsHeight + bonusHeight}px`
+            : `${headerHeight + projectsHeight + bonusHeight}px`,
+          transition: `max-height ${slideDurationMs}ms ease-out !important`,
         }}
         ref={ref}
       >
         <Container maxWidth="lg">
           {/* Header */}
-          <Box sx={{ textAlign: "center", mb: 6 }}>
+          <Box sx={{ textAlign: "center", mb: 6 }} ref={headerRef}>
             <Typography
               variant="h1"
               sx={{
@@ -142,7 +165,7 @@ export const Projects = forwardRef<HTMLElement, ProjectsProps>(
                 ? "transform, opacity, height"
                 : "",
               opacity: isProjectSelected ? 1 : 0,
-              height: detailsHeight,
+              height: 0,
             }}
           >
             {selectedProject && (
@@ -160,9 +183,10 @@ export const Projects = forwardRef<HTMLElement, ProjectsProps>(
               display: "flex",
               flexDirection: "column",
               gap: 8,
-              maxHeight: isProjectSelected ? 0 : "10000px",
-              transition: `max-height ${slideDurationMs}ms ease-out !important`,
+              height: {projectsHeight}
             }}
+            maxWidth="lg"
+            ref={projectsRef}
           >
             {projectData.map((project, index) => (
               <ProjectCard
