f7f9157fb274a90278a4091c100d4f890cf59292
Brandon Rivera-Melo <briveramelo@gmail.com>
2025-03-06 17:39:25 -0600

    fixed the no-padding issue on mobile for project cards

--- src/pages/sections/Projects/ProjectGroup.tsx
+++ src/pages/sections/Projects/ProjectGroup.tsx
@@ -66,7 +66,10 @@ const ProjectGroup: React.FC<ProjectGroupProps> = ({
       <Box
         sx={{
           display: "grid",
-          gridTemplateColumns: "repeat(auto-fit, minmax(390px, 1fr))",
+          gridTemplateColumns: {
+            xs: "repeat(auto-fit, minmax(350px, 1fr))",
+            sm: "repeat(auto-fit, minmax(390px, 1fr))",
+          },
           gap: "20px",
           justifyContent: "center",
         }}
